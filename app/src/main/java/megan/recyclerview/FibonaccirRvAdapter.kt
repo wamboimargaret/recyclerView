329e03b2602fb93fec7aaeec3866e9f7efcaae2d
wamboimargaret <margarettwamboi@gmail.com>
2023-05-23 17:41:56 +0300

    recycler view

---
+++ app/src/main/java/megan/recyclerview/FibonaccirRvAdapter.kt
@@ -0,0 +1,29 @@
+package megan.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+
+class NamesRvAdapter(private val numbs:List<Int>): Adapter<NamesViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NamesViewHolder {
+        var itemView = LayoutInflater.from(parent.context).inflate(R.layout.names_list_item,parent,false)
+        return NamesViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: NamesViewHolder, position: Int) {
+        val currentName =namesList.get(position)
+        holder.tvname.text=currentName
+    }
+
+}
+class NamesViewHolder(itemView: View):ViewHolder(itemView){
+    var tvname = itemView.findViewById<TextView>(R.id.tvNames)
+}
---
+++ app/src/main/java/megan/recyclerview/MainActivity.kt
@@ -0,0 +1,29 @@
+package megan.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var rvnames: RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        rvnames = findViewById(R.id.rvnames)
+    }
+
+    fun getFibonacciNumbers(limit: Int): List<Int> {
+        val fibonacciNumbers = mutableListOf(0, 1)
+        for (i in 2 until limit) {
+            fibonacciNumbers.add(fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2])
+        }
+        return fibonacciNumbers
+         }
+
+}
